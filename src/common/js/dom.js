c49882540ae78e039ca5dfa65b41741eb19a44f8
esnail <1293495173@qq.com>
2017-12-18 19:36:18 +0800

    chapter5 歌手详情页

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -22,3 +22,35 @@ export function getData (el, name, val) {
     return el.getAttribute(name)
   }
 }
+
+// css前缀兼容处理
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle (style) {
+  if (vendor === false) {
+    return false
+  }
+
+  if (vendor === 'stamdard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
