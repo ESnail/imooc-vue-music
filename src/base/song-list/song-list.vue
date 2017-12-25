7faa5fc6367957444becde0fa6ec7e78fcef2d31
esnail <1293495173@qq.com>
2017-12-25 11:14:00 +0800

    chapter6 播放器组件

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
-    <ul v-for="song in songs">
-      <li class="item">
+    <ul>
+      <li @click="selectItem(song, index)" v-for="(song, index) in songs" class="item">
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -20,6 +20,9 @@
       }
     },
     methods: {
+      selectItem (item, index) {
+        this.$emit('select', item, index)
+      },
       getDesc (song) {
         return `${song.singer} ▪ ${song.album}`
       }
