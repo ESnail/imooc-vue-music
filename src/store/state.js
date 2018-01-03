573d34eb41aa7e4454cc3dce5812b86fd03561df
esnail <1293495173@qq.com>
2018-01-03 17:03:05 +0800

    用户中心

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,5 @@
 import {playMode} from 'common/js/config'
-import {loadSearch, loadPlay} from 'common/js/cache'
+import {loadSearch, loadPlay, loadFavorite} from 'common/js/cache'
 
 const state = {
   singer: {},
@@ -20,7 +20,8 @@ const state = {
   // 排行榜详情
   topList: {},
   searchHistory: loadSearch(),
-  playHistory: loadPlay()
+  playHistory: loadPlay(),
+  favoriteList: loadFavorite()
 }
 
 export default state
